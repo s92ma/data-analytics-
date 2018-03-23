9601d92acf1b2f194f829dbc2779c17e48bbce05
s92ma <37000098+s92ma@users.noreply.github.com>
2018-03-22 21:51:53 -0400

    Update finalProject.R

--- finalProject.R
+++ finalProject.R
@@ -604,4 +604,5 @@ plot(testing.ts, col = "gray", main = "Forecasted Data for optimalARIMA Model")
 lines(c(1:length(testing.ts)), forecastedOptimalARIMA$mean, col = "blue")
 plot(c(testing.ts), forecastedOptimalARIMA$mean, main = "Forecasted Against Test Data Set (optimalARIMA)")
 
+# Save the predicted values to the file "output_team_9.csv".
 write.csv(forecastedOptimalARIMA$mean, file = "output_team_9.csv")
